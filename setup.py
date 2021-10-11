5294b878959e0b995c26b288e65964ace1be84b1
codybum <cbumgardner@gmail.com>
2021-10-11 18:50:26 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pycrescolib',
+    version='0.1',
+    packages=['src'],
+    url='http://cresco.io',
+    license='Apache 2.0',
+    author='Cresco Team',
+    author_email='info@cresco.io',
+    description='Python Cresco Client Library'
+)
---
+++ src/pycrescolib/clientlib.py
@@ -0,0 +1,47 @@
+from pyclientlib.admin import admin
+from pyclientlib.agents import agents
+from pyclientlib.dataplane import dataplane
+from pyclientlib.globalcontroller import globalcontroller
+from pyclientlib.logstreamer import logstreamer
+from pyclientlib.wc_interface import ws_interface
+from pyclientlib.messaging import messaging
+
+class clientlib(object):
+
+    def __init__(self,host, port, service_key):
+        self.host = host
+        self.port = port
+        self.service_key = service_key
+        self.ws_interface = ws_interface()
+        self.messaging = messaging(self.ws_interface)
+        self.agents = agents(self.messaging)
+        self.admin = admin(self.messaging)
+        self.globalcontroller = globalcontroller(self.messaging)
+
+    def connect(self):
+        try:
+            ws_url = 'ws://' + self.host + ':' + str(self.port) +'/api/apisocket'
+            isWSConnected = self.ws_interface.connect(ws_url)
+            return isWSConnected
+        except:
+            return False
+
+    def connected(self):
+        try:
+            return self.ws_interface.connected()
+        except:
+            return False
+
+    def close(self):
+        if self.ws_interface.connected():
+            self.ws_interface.close()
+
+    def get_dataplane(self, stream_name):
+        return dataplane(self.host, self.port, stream_name)
+
+    def get_logstreamer(self):
+        return logstreamer(self.host, self.port)
+
+
+
+
