a91314af53f4d3676e64d26c620f9840be2924e5
Vaiden Logan <william.logan1@uky.edu>
2024-02-21 09:14:34 -0500

    Fixed cresco_service_key mismatch

--- pycrescolib/wc_interface.py
+++ pycrescolib/wc_interface.py
@@ -7,10 +7,12 @@ class ws_interface(object):
     def __init__(self):
         self.url = None
         self.ws = None
+        self.cresco_service_key = None
 
-    def connect(self, url):
+    def connect(self, url, cresco_service_key):
         self.url = url
-        self.ws = create_connection(self.url, sslopt={"cert_reqs": ssl.CERT_NONE}, header={'cresco_service_key': 'abc-8675309'})
+        self.cresco_service_key = cresco_service_key
+        self.ws = create_connection(self.url, sslopt={"cert_reqs": ssl.CERT_NONE}, header={'cresco_service_key': self.cresco_service_key})
         return self.ws.connected
 
     def connected(self):
